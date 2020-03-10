4132bd9a94760f3fb44fa34a8c69ee0b039a9489
Migho <mi.sidorenko@gmail.com>
2020-03-10 10:05:27 +0200

    added_press_right_press_left

--- js/task.js
+++ js/task.js
@@ -4,14 +4,20 @@ import gallery from "./gallery-items.js";
 const galleryList = document.querySelector(".js-gallery");
 const modalWindow = document.querySelector(".js-lightbox");
 const closeButton = document.querySelector("button[data-action ='close-lightbox']");
-const list = creatElement(gallery);
-const image = document.querySelector(".lightbox__image");
 
-function creatElement({
+const image = document.querySelector(".lightbox__image");
+// let count = 0;
+let newUrl;
+let newAlt;
+
+
+
+
+const creatElement = ({
     preview,
     original,
     description
-}) {
+}) => {
     const galleryItemList = document.createElement("li");
     galleryItemList.classList.add('gallery__item');
     const galleryLink = document.createElement("a");
@@ -26,7 +32,7 @@ function creatElement({
     galleryLink.appendChild(galleryImg);
     return galleryItemList;
 }
-
+const list = creatElement(gallery);
 const makeImageList = gallery.map((item) => creatElement(item));
 
 galleryList.append(...makeImageList);
@@ -48,9 +54,9 @@ const assignmentAttribute = (src, alt) => {
 const closeModal = () => {
     assignmentAttribute("", "");
     modalWindow.classList.remove("is-open");
+    window.removeEventListener("keydown", changeImg)
 }
 closeButton.addEventListener("click", closeModal);
-
 window.addEventListener("keydown", e=> {
    
     if(e.key !== "Escape") {
@@ -58,8 +64,29 @@ window.addEventListener("keydown", e=> {
     } 
     closeModal();
 });
+const changeImg = (e)=>{
+   let count = gallery.findIndex(item=>item.original === newUrl); 
+    if(e.code === "ArrowRight"){
+        if(count=== galleryList.length -1){
+            return
+    } else{
+        count +=1
+    }
+}
+if (e.code === "ArrowLeft"){
+    if(count === galleryList.length +1){
+return
+    } else {
+        count -=1
+    }
+} 
 
+newUrl = gallery[count].original;
+newAlt = gallery[count].description;
+assignmentAttribute(newUrl, newAlt)
+}
 
+window.addEventListener("keydown", changeImg)
 modalWindow.addEventListener("click", e => {
 if(e.target === image){
     return
